efbb7a877473c71ddda1fb409c36c4c26f17b6a5
baby123456 <2118537295@qq.com>
2020-05-03 02:02:38 +0800

    add some

--- all.tcl
+++ all.tcl
@@ -17,14 +17,14 @@ set bitDir    "./Bitstreams"
 #--top
 #-----count inst_count(count_up,count_down)
 #-----shift inst_shift(shift_left,shift_right)
-set static "top"
-set pm1 "count"
-set init_pm1 inst_${pm1}
+set static top
+set pm1 count
+set inst_pm1 inst_count
 set pm1_v1 ${pm1}_up
 set pm1_v2 ${pm1}_down
 
-set pm2 "shift"
-set inst_pm2 inst_${pm2}
+set pm2 shift
+set inst_pm2 inst_shift
 set pm2_v1 ${pm2}_right
 set pm2_v2 ${pm2}_left
 
@@ -121,10 +121,10 @@ add_files $synthDir/Static/${static}_synth.dcp
 add_files $xdcDir/top_interwiser.xdc
 set_property USED_IN {implementation} [get_files $xdcDir/top_interwiser.xdc]
 add_file $synthDir/${pm2_v1}/${pm2}_synth.dcp
-set_property SCOPED_TO_CELLS {${inst_pm2}} [get_files $synthDir/${pm2_v1}/${pm2}_synth.dcp]
+set_property SCOPED_TO_CELLS {inst_shift} [get_files $synthDir/${pm2_v1}/${pm2}_synth.dcp]
 add_file $synthDir/${pm1_v1}/${pm1}_synth.dcp
-set_property SCOPED_TO_CELLS {${init_pm1}} [get_files $synthDir/${pm1_v1}/${pm1}_synth.dcp]
-link_design -mode default -reconfig_partitions {${inst_pm2} ${init_pm1}} -part ${device} -top ${static}
+set_property SCOPED_TO_CELLS {inst_count} [get_files $synthDir/${pm1_v1}/${pm1}_synth.dcp]
+link_design -mode default -reconfig_partitions {inst_count inst_shift} -part ${device} -top ${static}
 write_checkpoint -force $implDir/${init_config}/top_link_design.dcp
 opt_design
 write_checkpoint -force $implDir/${init_config}/top_opt_design.dcp 
@@ -146,12 +146,12 @@ write_checkpoint -force -cell ${inst_pm2} $implDir/${init_config}/${inst_pm2}_${
 file copy -force $implDir/${init_config}/${inst_pm2}_${pm2_v1}_route_design.dcp $dcpDir
 # 将布线结果中的inst_shift替换为blackbox
 update_design -cell ${inst_pm2} -black_box
-#锁定${init_pm1}的count_up布线
-lock_design -level logical -cell $init_pm1
-write_checkpoint -force -cell $init_pm1 $implDir/${init_config}/${init_pm1}_${pm1_v1}_route_design.dcp
-file copy -force $implDir/${init_config}/${init_pm1}_${pm1_v1}_route_design.dcp $dcpDir
+#锁定${inst_pm1}的count_up布线
+lock_design -level logical -cell $inst_pm1
+write_checkpoint -force -cell $inst_pm1 $implDir/${init_config}/${inst_pm1}_${pm1_v1}_route_design.dcp
+file copy -force $implDir/${init_config}/${inst_pm1}_${pm1_v1}_route_design.dcp $dcpDir
 # 将布线结果中的inst_count替换为blackbox
-update_design -cell ${init_pm1} -black_box
+update_design -cell ${inst_pm1} -black_box
 # 锁定inst_shift和inst_count替换为黑盒后的布线结果
 lock_design -level routing
 write_checkpoint -force $implDir/${init_config}/top_static.dcp
@@ -171,10 +171,10 @@ create_project -in_memory -part ${device}
 set_property board_part ${board} [current_project]
 add_files $dcpDir/top_static.dcp
 add_file $synthDir/${pm2_v2}/${pm2}_synth.dcp
-set_property SCOPED_TO_CELLS {${inst_pm2}} [get_files $synthDir/${pm2_v2}/${pm2}_synth.dcp]
+set_property SCOPED_TO_CELLS { inst_shift } [get_files $synthDir/${pm2_v2}/${pm2}_synth.dcp]
 add_file $synthDir/${pm1_v2}/${pm1}_synth.dcp
-set_property SCOPED_TO_CELLS {${init_pm1}} [get_files $synthDir/${pm1_v2}/${pm1}_synth.dcp]
-link_design -mode default -reconfig_partitions {${inst_pm2} ${init_pm1}} -part ${device} -top top
+set_property SCOPED_TO_CELLS { inst_count } [get_files $synthDir/${pm1_v2}/${pm1}_synth.dcp]
+link_design -mode default -reconfig_partitions { inst_count inst_shift } -part ${device} -top top
 write_checkpoint -force $implDir/${alter_config}/top_link_design.dcp
 opt_design
 write_checkpoint -force $implDir/${alter_config}/top_opt_design.dcp
@@ -195,26 +195,26 @@ lock_design -level logical -cell ${inst_pm2}
 write_checkpoint -force -cell ${inst_pm2} $implDir/${alter_config}/${inst_pm2}_${pm2_v2}_route_design.dcp
 file copy -force $implDir/${alter_config}/${inst_pm2}_${pm2_v2}_route_design.dcp $dcpDir
 #锁定inst_count的count_down布线
-lock_design -level logical -cell ${init_pm1}
-write_checkpoint -force -cell ${init_pm1} $implDir/${alter_config}/${init_pm1}_${pm1_v2}_route_design.dcp
-file copy -force $implDir/${alter_config}/${init_pm1}_${pm1_v2}_route_design.dcp $dcpDir
+lock_design -level logical -cell ${inst_pm1}
+write_checkpoint -force -cell ${inst_pm1} $implDir/${alter_config}/${inst_pm1}_${pm1_v2}_route_design.dcp
+file copy -force $implDir/${alter_config}/${inst_pm1}_${pm1_v2}_route_design.dcp $dcpDir
 puts "#HD: Implementation ${alter_config} complete\n"
 close_project
 
 #5、验证两种配置是否兼容
 puts "#HD: Running pr_verify between initial Configuration '${alter_config}' and subsequent configurations '${init_config}'"
-pr_verify -full_check -initial $implDir/${alter_config}/top_route_design.dcp -additional {$implDir/${init_config}/top_route_design.dcp}
+pr_verify -full_check -initial $implDir/${alter_config}/top_route_design.dcp -additional  $implDir/${init_config}/top_route_design.dcp
 
 #6、生成两种配置的bit和部分配置bit
 puts "	#HD: Running write_bitstream on ${alter_config}"
 open_checkpoint $implDir/${alter_config}/top_route_design.dcp 
 write_bitstream -force  $bitDir/${alter_config}_full -no_partial_bitfile
 write_bitstream -force  -cell ${inst_pm2} $bitDir/pblock_${inst_pm2}_${pm2_v2}_partial
-write_bitstream -force  -cell ${init_pm1} $bitDir/pblock_${init_pm1}_${pm1_v2}_partial
+write_bitstream -force  -cell ${inst_pm1} $bitDir/pblock_${inst_pm1}_${pm1_v2}_partial
 close_project 
 puts "	#HD: Running write_bitstream on ${init_config}"
 open_checkpoint $implDir/${init_config}/top_route_design.dcp 
 write_bitstream -force  $bitDir/${init_config}_full -no_partial_bitfile
 write_bitstream -force  -cell ${inst_pm2} $bitDir/pblock_${inst_pm2}_${pm2_v1}_partial
-write_bitstream -force  -cell ${init_pm1} $bitDir/pblock_${init_pm1}_${pm1_v1}_partial
+write_bitstream -force  -cell ${inst_pm1} $bitDir/pblock_${inst_pm1}_${pm1_v1}_partial
 close_project 
